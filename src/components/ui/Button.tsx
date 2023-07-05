e4de88717f0a0508cab45aec13bd9a3ea8648f90
Pillow <fagodfagod@gmail.com>
2023-07-05 18:40:01 +0300

    button arrow

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,6 +1,24 @@
-import type {FC, PropsWithChildren, HTMLAttributes} from 'react';
-import cn from 'clsx';
-import arrow from '../../assets/images/arrow.svg';
+import type { FC, PropsWithChildren, HTMLAttributes } from "react";
+import cn from "clsx";
+
+const Arrow: FC<HTMLAttributes<HTMLOrSVGElement>> = (props) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        id="&#209;&#129;&#209;&#130;&#209;&#128;&#209;&#150;&#208;&#187;&#208;&#186;&#208;&#176; &#208;&#177;&#209;&#150;&#208;&#187;&#208;&#176;"
+        d="M0.606602 1.3934L11.2132 12L0.606602 22.6066"
+        stroke-width="1.5"
+      />
+    </svg>
+  );
+};
 
 interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   variant?: "primary" | "secondary" | "outline";
@@ -13,13 +31,20 @@ export const Button: FC<PropsWithChildren<ButtonProps>> = ({
   ...props
 }) => {
   return (
-    <button className={cn("px-8 py-5 uppercase text-lg", {
-      "bg-primary text-white": variant === "primary",
-      "bg-black text-white": variant === "secondary",
-      "bg-gray text-black": variant === "outline",
-    }, className)} {...props}>
+    <button
+      className={cn(
+        "group px-8 py-5 uppercase text-lg flex items-center gap-6 transition-all",
+        {
+          "bg-primary text-white": variant === "primary",
+          "bg-black text-white border border-black hover:backdrop-blur[5px] hover:bg-gray-rgba hover:text-black": variant === "secondary",
+          "bg-gray text-black border border-gray hover:bg-primary hover:text-white": variant === "outline",
+        },
+        className
+      )}
+      {...props}
+    >
       {children}
-      <img className="inline-block ml-6 my-auto" src={arrow} alt="Arrow" />
+      <Arrow className="h-[18px] w-auto stroke-current group-hover:translate-x-[20%] transition-all" />
     </button>
   );
 };
